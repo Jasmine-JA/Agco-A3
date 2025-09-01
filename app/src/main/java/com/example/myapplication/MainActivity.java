f7270f98d40205669ae1419cb7fa5680b50321fc
Jasmine-JA <jasminejane.agco@hcdc.edu.ph>
2025-09-01 11:52:09 +0800

    third

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -21,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
 
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
+
+
+
+            
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
